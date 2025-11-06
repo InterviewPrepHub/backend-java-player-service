1811f06baf9df681a3aab50a2e71545a4909b2fa
Divya Paliwal <div.pali@gmail.com>
2025-11-06 14:06:56 +0530

    add test cases for implementation of search player

--- src/main/java/com/app/playerservicejava/repository/PlayerRepository.java
+++ src/main/java/com/app/playerservicejava/repository/PlayerRepository.java
@@ -1,7 +1,7 @@
 package com.app.playerservicejava.repository;
+
 import com.app.playerservicejava.model.Player;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
--- src/test/java/com/app/playerservicejava/PlayerServiceTest.java
+++ src/test/java/com/app/playerservicejava/PlayerServiceTest.java
@@ -12,15 +12,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
+import org.springframework.data.jpa.domain.Specification;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PlayerServiceTest {
@@ -41,12 +41,27 @@ public class PlayerServiceTest {
 
     @BeforeEach
     void setup() {
+        createSamplePlayer1();
+        createSamplePlayer2();
+    }
+
+    private void createSamplePlayer2() {
+        samplePlayer = new Player();
+        samplePlayer.setFirstName("John");
+        samplePlayer.setLastName("Johnson");
+        samplePlayer.setBirthCountry("USA");
+        samplePlayer.setBats("R");
+        samplePlayer.setDebut("2014-04-01");
+    }
+
+    private void createSamplePlayer1() {
         samplePlayer = new Player();
         samplePlayer.setFirstName("John");
         samplePlayer.setLastName("Doe");
         samplePlayer.setBirthYear("1985");
     }
 
+    //---------
     @Test
     void shouldReturnMappedPageForValidAdminRole() {
         Pageable pageable = PageRequest.of(0, 10);
@@ -100,5 +115,34 @@ public class PlayerServiceTest {
                 () -> playerService.getAllPlayersBasedOnRoles("   ", pageable));
 
         assertEquals("Role parameter cannot be null or empty", ex.getMessage());
+    }
+
+    //---------
+
+    @Test
+    void shouldReturnFilteredPlayersWithPagination() {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("birthYear").descending());
+        Page<Player> playerPage = new PageImpl<>(List.of(samplePlayer));
+
+        when(playerRepository.findAll(any(Specification.class), eq(pageable))).thenReturn(playerPage);
+
+        Page<Player> result = playerService.searchPlayers("USA", "R", "2014", "John", pageable);
+
+        assertEquals(1, result.getTotalElements());
+        assertEquals("Johnson", result.getContent().get(0).getLastName());
+        verify(playerRepository, times(1)).findAll(any(Specification.class), eq(pageable));
+    }
+
+    @Test
+    void shouldHandleEmptyFiltersGracefully() {
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<Player> playerPage = new PageImpl<>(List.of(samplePlayer));
+
+        when(playerRepository.findAll(any(Specification.class), eq(pageable))).thenReturn(playerPage);
+
+        Page<Player> result = playerService.searchPlayers(null, null, null, null, pageable);
+
+        assertEquals(1, result.getTotalElements());
+        verify(playerRepository, times(1)).findAll(any(Specification.class), eq(pageable));
     }
 }
